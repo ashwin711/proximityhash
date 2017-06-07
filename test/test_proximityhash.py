962bf0687f821fd7136a7de91e26122157914a40
Ashwin Nair <ashwin@near.co>
2017-06-07 23:37:16 +0530

    tests added

--- test/test_proximityhash.py
+++ test/test_proximityhash.py
@@ -1,29 +1,39 @@
 import unittest
-import georaptor
+import proximityhash
 
-class CombinationsTestCase(unittest.TestCase):
-    """Tests for `getCombinations`."""
-    def test_in_circle_check(self):
-        string = 'tdnu2'
-        combinations = set(['tdnu20', 'tdnu21', 'tdnu22', 'tdnu23', 'tdnu24', 'tdnu25', 'tdnu26', 'tdnu27', 'tdnu28', 'tdnu29',
-                        'tdnu2b', 'tdnu2c', 'tdnu2d', 'tdnu2e', 'tdnu2f', 'tdnu2g', 'tdnu2h', 'tdnu2j', 'tdnu2k', 'tdnu2m',
-                        'tdnu2n', 'tdnu2p', 'tdnu2q', 'tdnu2r', 'tdnu2s', 'tdnu2t', 'tdnu2u', 'tdnu2v', 'tdnu2w', 'tdnu2x',
-                        'tdnu2y', 'tdnu2z'])
+class in_circle_test_case(unittest.TestCase):
+    """Tests for `in_circle_check`."""
+    def test_in_circle_check_true(self):
+        expected = True
+        output = proximityhash.in_circle_check(12, 77, 12.1, 77, 100)
+        self.assertEqual(output, expected)
 
-        output = set(georaptor.getCombinations(string))
-        self.assertEqual(output, combinations)
+    def test_in_circle_check_false(self):
+        expected = True
+        output = proximityhash.in_circle_check(12, 77, 23, 87, 100)
+        self.assertEqual(output, expected)
 
-class CompressionTestCase(unittest.TestCase):
-    """Tests for `Geohash compression`."""
-    def test_compress(self):
-        geohashes = set(['tdnu20', 'tdnu21', 'tdnu22', 'tdnu23', 'tdnu24', 'tdnu25', 'tdnu26', 'tdnu27', 'tdnu28', 'tdnu29',
-                     'tdnu2b', 'tdnu2c', 'tdnu2d', 'tdnu2e', 'tdnu2f', 'tdnu2g', 'tdnu2h', 'tdnu2j', 'tdnu2k', 'tdnu2m',
-                     'tdnu2n', 'tdnu2p', 'tdnu2q', 'tdnu2r', 'tdnu2s', 'tdnu2t', 'tdnu2u', 'tdnu2v', 'tdnu2w', 'tdnu2x',
-                     'tdnu2y', 'tdnu2z'])
-        final_geohash = set(['tdnu2'])
+class centroid_test_case(unittest.TestCase):
+    """Tests for `get_centroid`."""
+    def test_get_centroid(self):
+        expected = (15, 15)
+        output = proximityhash.get_centroid(10,10,10,10)
+        self.assertEqual(output, expected)
 
-        output = georaptor.compress(geohashes)
-        self.assertEqual(output, final_geohash)
+class latlon_convert_test_case(unittest.TestCase):
+    """Tests for `convert_to_latlon`."""
+    def test_convert_to_latlon(self):
+        expected = (12.008993216059187, 77.0091941298557)
+        output = proximityhash.convert_to_latlon(1000.0,1000.0, 12.0, 77.0)
+        self.assertEqual(output, expected)
+
+class create_geohash_test_case(unittest.TestCase):
+    """Tests for `create_geohash`."""
+    def test_create_geohash(self):
+        expected = 'tdnu20t9,tdnu20t8,tdnu20t3,tdnu20t2,tdnu20mz,tdnu20mx,tdnu20tc,tdnu20tb,tdnu20td,tdnu20tf'
+        output = proximityhash.create_geohash(12.0, 77.0, 20.0, 8, georaptor_flag=False, minlevel=1, maxlevel=12)
+        self.assertEqual(output, expected)
+
 
 if __name__ == '__main__':
     unittest.main()
