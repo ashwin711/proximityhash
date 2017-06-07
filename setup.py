79cfe93cacb2df14fa0d46842ffff767f35e86c5
Ashwin Nair <ashwin@near.co>
2017-06-07 21:26:15 +0530

    version and dependencies updated

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ import setuptools
 setup(
   name = 'proximityhash',
   py_modules = ['proximityhash'],
-  version = '0.0.6',
+  version = '0.0.7',
   description = 'Geohashes in proximity',
   long_description = open('README.rst').read(),
   author = 'Ashwin Nair',
   author_email = 'ashwinnair.ua@gmail.com',
   license = "MIT",
   url = 'https://github.com/ashwin711/proximityhash',
-  download_url = 'https://github.com/ashwin711/proximityhash/tarball/0.0.6',
+  download_url = 'https://github.com/ashwin711/proximityhash/tarball/0.0.7',
   keywords = ['geohash', 'optimizer', 'compression', 'geo', 'latitude', 'longitude', 'coordinates', 'proximity', 'circle'],
   classifiers = [
     'Development Status :: 4 - Beta',
@@ -25,7 +25,7 @@ setup(
   install_requires = [
 	'clint',
 	'argparse',
-    'georaptor',
+    'georaptor>=2.0.3',
     'Geohash'
   ],
   entry_points='''
