ed66089ec52f0deee8e8be37bf40a5e6dbfba163
Ashwin Nair <ashwin@near.co>
2017-06-07 17:28:51 +0530

    geohash library modified

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ import setuptools
 setup(
   name = 'proximityhash',
   py_modules = ['proximityhash'],
-  version = '0.0.4',
+  version = '0.0.5',
   description = 'Geohashes in proximity',
   long_description = open('README.rst').read(),
   author = 'Ashwin Nair',
   author_email = 'ashwinnair.ua@gmail.com',
   license = "MIT",
   url = 'https://github.com/ashwin711/proximityhash',
-  download_url = 'https://github.com/ashwin711/proximityhash/tarball/0.0.4',
+  download_url = 'https://github.com/ashwin711/proximityhash/tarball/0.0.5',
   keywords = ['geohash', 'optimizer', 'compression', 'geo', 'latitude', 'longitude', 'coordinates', 'proximity', 'circle'],
   classifiers = [
     'Development Status :: 4 - Beta',
@@ -26,7 +26,7 @@ setup(
 	'clint',
 	'argparse',
     'georaptor',
-    'geohash'
+    'Geohash'
   ],
   entry_points='''
 	[console_scripts]
