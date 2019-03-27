bc0426f7b65f7426ff18f74538d1317954c7f009
Trevor Rowe <tr968114@ohio.edu>
2019-03-27 19:26:23 -0400

    Fixed missing set() in call to georaptor

--- proximityhash.py
+++ proximityhash.py
@@ -83,7 +83,7 @@ def create_geohash(latitude, longitude, radius, precision, georaptor_flag=False,
         geohashes += [Geohash.encode(point[0], point[1], precision)]
 
     if georaptor_flag:
-        georaptor_out = georaptor.compress(geohashes, int(minlevel), int(maxlevel))
+        georaptor_out = georaptor.compress(set(geohashes), int(minlevel), int(maxlevel))
         if __name__ != '__main__':
             return georaptor_out
         return ','.join(georaptor_out)
